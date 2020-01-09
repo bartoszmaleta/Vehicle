bb96c3bf2926253c16494393bdfdd10b5bafa8fa
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-09 15:29:03 +0100

    Add accelerate()

--- src/app/Octavia.java
+++ src/app/Octavia.java
@@ -14,7 +14,32 @@ public class Octavia extends Car {
         super("Octavia", "hatchback", 5, 5, 6, false);
         this.roadServiceMonths = roadServiceMonths;
     }
+    
+    public void accelerate (int rate) {
+        int newVelocity = getCurrentVelocity() + rate;
+        if (newVelocity == 0) {
+            stop();
+        } else {
+            if (newVelocity > 0 && newVelocity <= 10) {
+                chagneGear(1);
+            } else if (newVelocity > 10 && newVelocity <= 20) {
+                chagneGear(2);
+            } else if (newVelocity > 20 && newVelocity <= 30) {
+                chagneGear(3);
+            } else if (newVelocity > 30 && newVelocity <= 40) {
+                chagneGear(4);
+            } else if (newVelocity > 40 && newVelocity <= 50) {
+                chagneGear(5);
+            } else if (newVelocity > 50) {
+                chagneGear(6);
+            }
+        }
 
-        
+        if (newVelocity > 0) {
+            changedVelocity(newVelocity, getCurrentDirection());
+        }
+    }
+
+    
     
 }
