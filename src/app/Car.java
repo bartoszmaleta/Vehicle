1a0298945f5684a9b9535d64659c92fe9e45a561
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-01-09 15:17:39 +0100

    Add contructor, fields, methods

---
+++ src/app/Car.java
@@ -0,0 +1,36 @@
+package app;
+
+public class Car extends Vehicle {
+    private int wheels;
+    private int doors;
+    private int gears;
+    private boolean isManuel;
+
+    private int currentGear;
+
+    public Car(String name, String size, int wheels, int doors, int gears, boolean isManuel) {
+        super(name, size);
+        this.wheels = wheels;
+        this.doors = doors;
+        this.gears = gears;
+        this.isManuel = isManuel;
+        this.currentGear = 1;
+    }
+
+    // public int getCurrentGear() {
+        // return currentGear;
+    // }
+
+    public void chagneGear (int currentGear) {
+        this.currentGear = currentGear;
+        System.out.println("Car.setCurrentGear(): Changed to " + this.currentGear + " gear.");
+    }
+
+    public void changedVelocity(int speed, int direction) {
+        move(speed, direction);
+        System.out.println("Car.changeVelocity() : Velocity " + speed + " direction " + direction);
+    }
+
+    
+    
+}
